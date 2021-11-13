d1f1b2b7e6e8ec6ac73a41591b661c8f5b4ea857
Ryota Fujii <rs7328ry@gmail.com>
2021-11-13 19:53:18 +0900

    変換形式

--- emf_to_tiff変換/convert_image.py
+++ emf_to_tiff変換/convert_image.py
@@ -5,10 +5,11 @@ from PIL import Image
 raw_data_path = "./raw_data/"
 converted_data_path = "./converted_data/"
 
-files = sorted((glob.glob(raw_data_path+"/*.out")))
+files = sorted((glob.glob(raw_data_path+"/*.emf")))
 filenames = []
 for file in files:
     filenames.append(os.path.splitext(os.path.basename(file))[0])
 
 for filename in filenames:
-  Image.open(raw_data_path+filename+".emf").save(converted_data_path+filename+".tiff",quality=90)
+  Image.open(raw_data_path+filename+".emf").save(converted_data_path+filename+".png",quality=90)
+  print("(｀・ω・´)")
