c4ea3d3e33c3dfb6aba72a34b47471a2e81277c1
Ryota Fujii <rs7328ry@gmail.com>
2021-11-19 17:15:05 +0900

    遷移確率とExcited stateの紐付け&小さいfの遷移確率の消去

--- TD-DFTデータ処理/convert_td_dft.py
+++ TD-DFTデータ処理/convert_td_dft.py
@@ -1,10 +1,17 @@
 import os 
 import pandas as pd
 import glob
-
+from pathlib import Path
 
 raw_data_path = "./raw_data/"
-exchanged_data_path = "./converted_data/"
+converted_data_path = "./converted_data/"
+prob_path = "prob/"
+graph_path = "graph/"
+
+dir_s = Path(converted_data_path + graph_path)
+dir_p = Path(converted_data_path + prob_path)
+dir_s.mkdir(parents=True, exist_ok=True)
+dir_p.mkdir(parents=True, exist_ok=True)
 
 files = sorted((glob.glob(raw_data_path+"/*.out")))
 filenames = []
@@ -17,33 +24,54 @@ for i in range(len(filenames)):
   filename = filenames[i]
   with open(raw_data_path+filename+".out") as f:
     lines = f.readlines()
-    ex_st_lines = [line for line in lines if "Excited State" and "nm" in line]
-    excited_State_strip = [line.strip() for line in ex_st_lines]
-    excited_State_strip = [line for line in excited_State_strip if "Excited State"in line]
-    df = pd.DataFrame(excited_State_strip)
-    df_split = df[0].str.split(expand=True)
-    df_s = df_split.iloc[:,[2,4,6,8]]
-    converted_df = df_s.rename(columns={2:"num", 4:"eV", 6:"nm",8:"f" })
-    converted_df["f"] = converted_df["f"].str.strip("f=")
-    converted_df["num"] = converted_df["num"].str.strip(":")
-    short_converted_df = converted_df[converted_df["f"].astype(float) >= 0.03]
+    new_lines =[]
+    r_start = False
+    for line in lines:
+      if "Excitation energies and oscillator strengths" in line:
+        r_start = True
+      elif " SavETr:" in line:
+        r_start = False
+        break
+      elif r_start == True:
+        new_lines.append(line.rstrip("\n"))
 
-    converted_df.to_csv(exchanged_data_path + filename + "_graph" +".csv")
-    short_converted_df.to_csv(exchanged_data_path + filename + "_shortgraph" +".csv")
+    excited_num = 0
+    excited_state = []
+    excited_nums = []
+    excited_list = []
+    for n_line in new_lines:
+      if "Excited State" in n_line and "nm" in n_line:
+        excited_num += 1
+        excited_state.append(n_line)
 
-    ex_st_probab = [line for line in lines if "->" or "<-"]
-    excited_probab_strip = [line.strip() for line in ex_st_probab]
-    excited_probab_strip = [line for line in excited_probab_strip if "->" in line]
-    for j in range(len(excited_probab_strip)):
-      excited_probab_strip[j] = excited_probab_strip[j].replace("->", " -> ")
-    df2 = pd.DataFrame(excited_probab_strip)
-    df2_split = df2[0].str.split(expand=True)
-    
-    df2_split["%"] = (df2_split[3].astype(float))**2*2*100
-    df2_split["round_%"] = df2_split["%"].round(2)
-    df2_split.to_csv(exchanged_data_path + filename + "_probab" +".csv")
+      if "->" in n_line or "<-" in n_line:
+        excited_nums.append(excited_num)
+        excited_list.append(n_line)
+
+    df_prob = pd.DataFrame(excited_list, index=None)
+    df_prob = df_prob[0].str.split(expand=True)
+    df_prob["Excited_State"] = pd.DataFrame(excited_nums)
+    df_prob["%"] = (df_prob[3].astype(float))**2*2*100
+    df_prob["round_%"] = df_prob["%"].round(1)
+
+    tmp_df_state = pd.DataFrame(excited_state, index=None)
+    tmp_df_state = tmp_df_state[0].str.split(expand=True)
+    df_state = tmp_df_state.iloc[:,[2,4,6,8]]
+    df_state = df_state.rename(columns={2:"num", 4:"eV", 6:"nm",8:"f" })
+    df_state["f"] = df_state["f"].str.strip("f=")
+    df_state["num"] = df_state["num"].str.strip(":")
+
+    short_df_state = df_state[df_state["f"].astype(float) >= 0.03]
+    short_df_list = list(short_df_state["num"].astype(int))
+
+    short_df_prob = df_prob[df_prob["Excited_State"].isin(short_df_list)]
+
+    df_state.to_csv(converted_data_path + graph_path + filename +".csv")
+    short_df_state.to_csv(converted_data_path + graph_path + filename +"_short.csv")
+
+    df_prob.to_csv(converted_data_path + prob_path + filename +".csv")
+    short_df_prob.to_csv(converted_data_path + prob_path + filename + "_short.csv")
+
 
 
 print("　(´･ω･`)\n＿(__つ/￣￣￣/＿\n　　＼/　　　　 /\n　　　 ￣￣￣￣\n\n　(´･ω･`)\n＿(　つ　ミ　　ﾊﾞﾀﾝｯ\n　　＼￣￣￣＼ミ\n　　　 ￣￣￣￣\n\n　(´･ω･`)\n＿(　　　)\n　　＼￣￣￣＼')\n変換処理が終わりました")
-
-  
